64f5bb38c11ef18b70765c72793e83c6ddf9aa97
daridev <darideveloper@gmail.com>
2025-09-05 19:12:26 -0600

    feat (core/models): create and register api testing models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from core import models
+
+
+@admin.register(models.Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ["name", "description", "endpoint_base"]
+    search_fields = ["name", "description", "endpoint_base"]
+    list_filter = ["created_at", "updated_at"]
+    ordering = ["name"]
+
+
+@admin.register(models.Method)
+class MethodAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    search_fields = ["name"]
+    list_filter = ["created_at", "updated_at"]
+    ordering = ["name"]
+
+
+@admin.register(models.Endpoint)
+class EndpointAdmin(admin.ModelAdmin):
+    list_display = ["name", "description", "endpoint"]
+    search_fields = ["name", "description", "endpoint"]
+    list_filter = ["created_at", "updated_at", "project", "methods"]
+    ordering = ["project", "name"]
--- core/models.py
+++ core/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    endpoint_base = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = "Project"
+        verbose_name_plural = "Projects"
+
+
+class Method(models.Model):
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = "Method"
+        verbose_name_plural = "Methods"
+
+
+class Endpoint(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    endpoint = models.CharField(max_length=255)
+    methods = models.ManyToManyField(Method)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Endpoint"
+        verbose_name_plural = "Endpoints"
