a0bfd7cc7c8fcd7b2f52cffeac8a7299d0e0bc87
daridev <darideveloper@gmail.com>
2025-09-08 16:59:34 -0600

    fix (settings/templates): add core templates folder

--- project/settings.py
+++ project/settings.py
@@ -76,7 +76,7 @@ ROOT_URLCONF = "project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "core" / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
